4ccb5e1d008af2c82a48d3aa00cbdadb4b339992
IvanNunez25 <ivan25606@gmail.com>
2023-09-28 10:52:44 -0600

    Primera etapa

--- 2023_09_28_appVentas_ExpReg/main.m
+++ 2023_09_28_appVentas_ExpReg/main.m
@@ -10,7 +10,27 @@
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
-        NSLog(@"Hello, World!");
+        NSLog(@"Mi ferretería");
+        
+        NSError *error = nil;
+        
+        NSString *fileName = @"/Users/ivanovicxnunez/Documents/EJESAD 23/venta1.txt";
+        NSString *contenido = [[NSString alloc] initWithContentsOfFile:fileName encoding:NSASCIIStringEncoding error:&error];
+        
+        NSRegularExpression *regex = [[NSRegularExpression alloc] initWithPattern:@"Orden: \\d+" options:0 error:&error];
+        
+        long nMatches = [regex numberOfMatchesInString:contenido options:0 range:NSMakeRange(0, contenido.length)];
+        
+        NSLog(@"Número de coincidencias: %ld", nMatches);
+        
+        NSArray *matches = [regex matchesInString:contenido options:0 range:NSMakeRange(0, contenido.length)];
+        
+        for(NSTextCheckingResult *match in matches){
+            NSString *matchText = [contenido substringWithRange:[match range]];
+            NSLog(@"Orden encontrada: %@", matchText);
+        }
+        
+        
     }
     return 0;
 }
