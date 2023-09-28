aed5239929fbe7fcc174807ce1de6b394a30bb37
IvanNunez25 <ivan25606@gmail.com>
2023-09-28 10:29:04 -0600

    Initial Commit

---
+++ 2023_09_28_appVentas_ExpReg/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  2023_09_28_appVentas_ExpReg
+//
+//  Created by Ivanovicx Nuñez on 28/09/23.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
